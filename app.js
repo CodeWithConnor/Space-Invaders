f8c042d91b7ce646b88ff600a5e702b30ac3b4d3
Connor <hello@connorhanks.com>
2022-09-03 00:30:20 +0100

    added motion for invaders, collision detection

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const grid = document.querySelector(".grid");
+const resultsDisplay = document.querySelector(".results");
+let currentShooterIndex = 202;
+let gridWidth = 15;
+let direction = 1;
+let invadersId;
+let goingRight = true; // default direction is right
 
 for (let i = 0; i < 225; i++) {
     const square = document.createElement("div");
@@ -20,3 +26,70 @@ function draw() {
 }
 
 draw();
+
+function remove() {
+    for (let i = 0; i < alienInvaders.length; i++) {
+        squares[alienInvaders[i]].classList.remove("invader");
+    }
+}
+
+squares[currentShooterIndex].classList.add("shooter");
+
+function moveShooter(e) {
+    squares[currentShooterIndex].classList.remove("shooter");
+    switch (e.key) {
+        case "a":
+            if (currentShooterIndex % gridWidth !== 0) currentShooterIndex -= 1;
+            // we know that the shooter is at the right edge of the grid
+            break;
+        case "d":
+            if (currentShooterIndex % gridWidth < 14) currentShooterIndex += 1;
+            // we know that the shooter is at the left edge of the grid
+            break;
+    }
+    squares[currentShooterIndex].classList.add("shooter");
+}
+document.addEventListener("keydown", moveShooter);
+
+function moveInvaders() {
+    const leftEdge = alienInvaders[0] % gridWidth === 0;
+    const rightEdge =
+        alienInvaders[alienInvaders.length - 1] % gridWidth === gridWidth - 1;
+    remove();
+
+    // if invader is at right edge and going right
+    if (rightEdge && goingRight) {
+        // move invader down
+        for (let i = 0; i < alienInvaders.length; i++) {
+            // for each invader, add an entire width (15) and then -1
+            alienInvaders[i] += gridWidth + 1;
+            direction = -1;
+            goingRight = false;
+        }
+    }
+
+    // handle opposite direction
+    if (leftEdge && !goingRight) {
+        for (let i = 0; i < alienInvaders.length; i++) {
+            alienInvaders[i] += gridWidth - 1;
+            direction = 1;
+            goingRight = true;
+        }
+    }
+
+    // loop over each invader and assign them a different position
+    for (let i = 0; i < alienInvaders.length; i++) {
+        alienInvaders[i] += direction;
+    }
+
+    draw();
+
+    // if the shooter is hit by an invader (if the div has both the invader and shooter class)
+    if (squares[currentShooterIndex].classList.contains("invader", "shooter")) {
+        resultsDisplay.innerHTML = "Game Over";
+        // stops moving invaders
+        clearInterval(invadersId);
+    }
+}
+
+invadersId = setInterval(moveInvaders, 500);
