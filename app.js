66460062d79a763c2253741b7d0f61e0ad7c2d3b
Connor <hello@connorhanks.com>
2022-09-09 16:59:37 +0100

    fixed bullet collision, added game over screen, added win screen, added pause menu, added key toggle

--- app.js
+++ app.js
@@ -1,20 +1,45 @@
-const keyLeft = 65; // A
-const keyRight = 68; // D
-const keySpace = 32; // Space
-const gameWidth = 800;
-const gameHeight = 600;
-const state = {
-    xPos: 0,
-    yPos: 0,
-    moveLeft: false,
-    moveRight: false,
+// Keys
+const KEY_UP = 38;
+const KEY_DOWN = 40;
+const KEY_RIGHT = 68;
+const KEY_LEFT = 65;
+const KEY_SPACE = 32;
+const KEY_ESC = 27;
+
+// Game properties
+const GAME_WIDTH = 800;
+const GAME_HEIGHT = 600;
+
+// UI
+const banner = document.querySelector("body > div > header");
+const menu = document.querySelector(".menu");
+
+// Misc
+
+const STATE = {
+    x_pos: 0,
+    y_pos: 0,
+    move_right: false,
+    move_left: false,
     shoot: false,
     lasers: [],
-    spaceshipWidth: 50,
+    enemyLasers: [],
+    enemies: [],
+    spaceship_width: 40,
+    enemy_width: 50,
     cooldown: 0,
+    number_of_enemies: 16,
+    enemy_cooldown: 0,
+    gameOver: false,
+    gameWon: true,
+    score: 0,
 };
 
 // General purpose functions
+// function playSound(sound) {
+//     let soundFile = () => new Audio("").play()
+// }
+
 function setPosition($element, x, y) {
     $element.style.transform = `translate(${x}px, ${y}px)`;
 }
@@ -24,120 +49,237 @@ function setSize($element, width) {
     $element.style.height = "auto";
 }
 
-// used to check if player is in bounds of game div
 function bound(x) {
-    if (x >= gameWidth - state.spaceshipWidth) {
-        state.xPos = gameWidth - state.spaceshipWidth;
-        return gameWidth - state.spaceshipWidth;
+    if (x >= GAME_WIDTH - STATE.spaceship_width) {
+        STATE.x_pos = GAME_WIDTH - STATE.spaceship_width;
+        return GAME_WIDTH - STATE.spaceship_width;
     }
     if (x <= 0) {
-        state.xPos = 0;
+        STATE.x_pos = 0;
         return 0;
     } else {
         return x;
     }
 }
 
-// removes laser from DOM and state
-function deleteLaser(lasers, laser, $laser) {
-    const index = lasers.indexOf(laser);
-    lasers.splice(index, 1);
-    $container.removeChild($laser);
+function collideRect(rect1, rect2) {
+    return !(
+        rect2.left > rect1.right ||
+        rect2.right < rect1.left ||
+        rect2.top > rect1.bottom ||
+        rect2.bottom < rect1.top
+    );
+}
+
+// Enemy
+function createEnemy($container, x, y) {
+    const $enemy = document.createElement("img");
+    $enemy.src = "img/ufo.svg";
+    $enemy.className = "enemy";
+    $container.appendChild($enemy);
+    const enemy_cooldown = Math.floor(Math.random() * 100);
+    const enemy = { x, y, $enemy, enemy_cooldown };
+    STATE.enemies.push(enemy);
+    setSize($enemy, STATE.enemy_width);
+    setPosition($enemy, x, y);
+}
+
+function updateEnemies($container) {
+    const dx = Math.sin(Date.now() / 1000) * 40;
+    const dy = Math.cos(Date.now() / 1000) * 30;
+    const enemies = STATE.enemies;
+    for (let i = 0; i < enemies.length; i++) {
+        const enemy = enemies[i];
+        var a = enemy.x + dx;
+        var b = enemy.y + dy;
+        setPosition(enemy.$enemy, a, b);
+        enemy.cooldown = Math.random(0, 100);
+        if (enemy.enemy_cooldown == 0) {
+            createEnemyLaser($container, a, b);
+            enemy.enemy_cooldown = Math.floor(Math.random() * 50) + 100;
+        }
+        enemy.enemy_cooldown -= 0.5;
+    }
 }
 
 // Player
 function createPlayer($container) {
-    state.xPos = gameWidth / 2;
-    state.yPos = gameHeight - 50;
+    STATE.x_pos = GAME_WIDTH / 2;
+    STATE.y_pos = GAME_HEIGHT - 50;
     const $player = document.createElement("img");
-    $player.src = "img/spaceship.png";
+    $player.src = "img/spaceship.svg";
     $player.className = "player";
     $container.appendChild($player);
-    setPosition($player, state.xPos, state.yPos);
-    setSize($player, state.spaceshipWidth);
+    setPosition($player, STATE.x_pos, STATE.y_pos);
+    setSize($player, STATE.spaceship_width);
 }
 
 function updatePlayer() {
-    if (state.moveLeft) {
-        state.xPos -= 3;
+    if (STATE.move_left) {
+        STATE.x_pos -= 3;
     }
-    if (state.moveRight) {
-        state.xPos += 3;
+    if (STATE.move_right) {
+        STATE.x_pos += 3;
     }
-    if (state.shoot && state.cooldown === 0) {
+    if (STATE.shoot && STATE.cooldown == 0) {
         createLaser(
             $container,
-            state.xPos,
-            state.spaceshipWidth / 2,
-            state.yPos
+            STATE.x_pos - STATE.spaceship_width / 2,
+            STATE.y_pos
         );
-        state.cooldown = 30;
+        STATE.cooldown = 30;
     }
     const $player = document.querySelector(".player");
-    // ran through bound() to check if player is in bounds of game div
-    setPosition($player, bound(state.xPos), state.yPos - 10);
-    if (state.cooldown > 0) {
-        state.cooldown -= 0.5;
+    setPosition($player, bound(STATE.x_pos), STATE.y_pos - 10);
+    if (STATE.cooldown > 0) {
+        STATE.cooldown -= 0.5;
     }
 }
 
+// Player Laser
 function createLaser($container, x, y) {
     const $laser = document.createElement("img");
     $laser.src = "img/laser.png";
     $laser.className = "laser";
     $container.appendChild($laser);
     const laser = { x, y, $laser };
-    state.lasers.push(laser);
+    STATE.lasers.push(laser);
     setPosition($laser, x, y);
 }
 
-// Responsible for moving laser across the screen
 function updateLaser($container) {
-    const lasers = state.lasers;
+    const lasers = STATE.lasers;
     for (let i = 0; i < lasers.length; i++) {
         const laser = lasers[i];
         laser.y -= 2;
-
-        // deletes laser once it goes off screen
-        if (laser.y < -500) {
+        if (laser.y < 0) {
             deleteLaser(lasers, laser, laser.$laser);
         }
-
         setPosition(laser.$laser, laser.x, laser.y);
+        const laser_rectangle = laser.$laser.getBoundingClientRect();
+        const enemies = STATE.enemies;
+        for (let j = 0; j < enemies.length; j++) {
+            const enemy = enemies[j];
+            const enemy_rectangle = enemy.$enemy.getBoundingClientRect();
+            // Check for collision between ufo/laser
+            if (collideRect(enemy_rectangle, laser_rectangle)) {
+                deleteLaser(lasers, laser, laser.$laser);
+                const index = enemies.indexOf(enemy);
+                enemies.splice(index, 1);
+                $container.removeChild(enemy.$enemy);
+                STATE.score++;
+                banner.innerHTML = `Space Invaders | Score: ${STATE.score}/18`;
+            }
+        }
     }
+}
+
+// Enemy Laser
+function createEnemyLaser($container, x, y) {
+    const $enemyLaser = document.createElement("img");
+    $enemyLaser.src = "img/enemyLaser.png";
+    $enemyLaser.className = "enemyLaser";
+    $container.appendChild($enemyLaser);
+    const enemyLaser = { x, y, $enemyLaser };
+    STATE.enemyLasers.push(enemyLaser);
+    setPosition($enemyLaser, x, y);
+}
+
+function updateEnemyLaser($container) {
+    const enemyLasers = STATE.enemyLasers;
+    for (let i = 0; i < enemyLasers.length; i++) {
+        const enemyLaser = enemyLasers[i];
+        enemyLaser.y += 2;
+        if (enemyLaser.y > GAME_HEIGHT - 30) {
+            deleteLaser(enemyLasers, enemyLaser, enemyLaser.$enemyLaser);
+        }
+        const enemyLaser_rectangle =
+            enemyLaser.$enemyLaser.getBoundingClientRect();
+        const spaceship_rectangle = document
+            .querySelector(".player")
+            .getBoundingClientRect();
+        if (collideRect(spaceship_rectangle, enemyLaser_rectangle)) {
+            STATE.gameOver = true;
+        }
+        setPosition(
+            enemyLaser.$enemyLaser,
+            enemyLaser.x + STATE.enemy_width / 2,
+            enemyLaser.y + 15
+        );
+    }
+}
+
+// Delete Laser
+function deleteLaser(lasers, laser, $laser) {
+    const index = lasers.indexOf(laser);
+    lasers.splice(index, 1);
+    $container.removeChild($laser);
 }
 
 // Key Presses
 function KeyPress(event) {
-    if (event.keyCode === keyRight) {
-        state.moveRight = true;
-    } else if (event.keyCode === keyLeft) {
-        state.moveLeft = true;
-    } else if (event.keyCode === keySpace) {
-        state.shoot = true;
+    if (event.keyCode === KEY_RIGHT) {
+        STATE.move_right = true;
+    } else if (event.keyCode === KEY_LEFT) {
+        STATE.move_left = true;
+    } else if (event.keyCode === KEY_SPACE) {
+        STATE.shoot = true;
+    } else if (event.keyCode === KEY_ESC) {
+        menu.style.display = "block";
     }
 }
 
 function KeyRelease(event) {
-    if (event.keyCode === keyRight) {
-        state.moveRight = false;
-    } else if (event.keyCode === keyLeft) {
-        state.moveLeft = false;
-    } else if (event.keyCode === keySpace) {
-        state.shoot = false;
+    if (event.keyCode === KEY_RIGHT) {
+        STATE.move_right = false;
+    } else if (event.keyCode === KEY_LEFT) {
+        STATE.move_left = false;
+    } else if (event.keyCode === KEY_SPACE) {
+        STATE.shoot = false;
     }
 }
 
-// Main update function
+// Main Update Function
 function update() {
     updatePlayer();
-    updateLaser();
+    updateEnemies($container);
+    updateLaser($container);
+    updateEnemyLaser($container);
+
     window.requestAnimationFrame(update);
+
+    if (STATE.gameOver) {
+        hideMenu();
+        document.querySelector(".lose").style.display = "block";
+    }
+    if (STATE.enemies.length == 0) {
+        hideMenu();
+        gameWon = true;
+        document.querySelector(".win").style.display = "block";
+    }
 }
 
-// Init. Game
+function restartGame() {
+    window.location.reload();
+}
+
+function hideMenu() {
+    menu.style.display = "none";
+}
+
+function createEnemies($container) {
+    for (var i = 0; i <= STATE.number_of_enemies / 2; i++) {
+        createEnemy($container, i * 80, 100);
+    }
+    for (var i = 0; i <= STATE.number_of_enemies / 2; i++) {
+        createEnemy($container, i * 80, 180);
+    }
+}
+
+// Initialize the Game
 const $container = document.querySelector(".main");
 createPlayer($container);
+createEnemies($container);
 
 // Key Press Event Listener
 window.addEventListener("keydown", KeyPress);
